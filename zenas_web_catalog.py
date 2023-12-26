2f4bbfc71b3785a7453836560b01cdd7fc5d9ea2
Shan-MH <154539791+Shan-MH@users.noreply.github.com>
2023-12-26 12:16:52 -0600

    Update zenas_web_catalog.py

--- zenas_web_catalog.py
+++ zenas_web_catalog.py
@@ -13,7 +13,7 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
 # put data into dataframe 
-df = pandas.dataframe(my_catalog)
+df = pandas.DataFrame(my_catalog)
 
 # temp write the dataframe to the page to see what we're working with 
 streamlit.write(df)
